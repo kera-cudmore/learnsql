5a2b0350e8d3d1718feeb577fa7125452ef4890b
Kera Cudmore <kera.cudmore@hotmail.com>
2022-04-29 18:43:22 +0000

    adding a row to a table

--- my-sql-from-python.py
+++ my-sql-from-python.py
@@ -1,7 +1,8 @@
 import os
+import datetime
 import pymysql
 
-# Get username from Gitpod workspace
+# Get username from Gitpod workspace - saved as an environment variable
 # Modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
@@ -14,10 +15,11 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        row = ("Bob", 21, "1990-02-06 23:04")
+        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
+        #note that the above will still display a warning (not error) if the
+        #table already exists
 finally:
-    #Close the connection, regardless of whether the above was successful
+    # Close the connection, regardless of whether the above was successful
     connection.close()
